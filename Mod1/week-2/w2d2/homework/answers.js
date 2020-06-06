ea950bcd5dcfd6041e2a2ce2908ae2ac23eb59ec
PSCBUS_109 <pscbus_109@PSCBUS-109.local>
2020-06-06 12:33:42 -0400

    finished easy going

---
+++ Mod1/week-2/w2d2/homework/answers.js
@@ -0,0 +1,54 @@
+
+/////////////////////
+//Getting Started //
+///////////////////
+//1.  created homework folder inside w2d3
+
+//2.  created javascript file insde homework folder nameed answers.js
+
+//3.  keep organized - moving forward use a commented header for each section
+
+//4.  TEST WORK! make sure code works node answers.js
+
+//5.  Frustrated or stuck for too long, make a comment move on come back to it later
+
+//6.  After each section (even if you are not fully done with it) add and commit work
+
+//7.  When done with homework, push it to Github and make an issue on our repo
+
+//Final note: I am not alone! Everyone is working on this as well Slack, Zoom, or  
+//meet with IA(Jose C.) to discuss sticking points
+
+///////////////////////
+//Part I: HTML & CSS//
+/////////////////////
+//Watch Videos and  give ranking based on comfort with material
+
+//HTML INTRO (tags, content, attributes, semantic HTML)
+//  5 - I got this! I am ready to code
+//HTML Elements (headers, navigation, lists, sections, etc.) and Media (images, audio, videos)
+//  5 - I got this! I am ready to code
+//CSS Intro (including css files in HTML,elements)
+//  5 - I got this! I am ready to code
+//CSS Basics (colors, background, font properties)
+//  5 - I got this! I am ready to code
+
+
+////////////////////////////
+//Part II: JavaScript Reps//
+///////////////////////////
+for (i = 1; i <= 20; i++){
+    console.log(i)
+}
+
+//Easy Going
+////////////////////////
+
+//Get Even
+//////////////////////
+
+
+
+
+
+
